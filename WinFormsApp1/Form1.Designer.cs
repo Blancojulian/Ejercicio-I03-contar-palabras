2ebae8764891f51e13c562ee1414a3b3080a1961
Blancojulian <julianblanco963@hotmail.com>
2022-09-12 21:59:55 -0300

    form

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtb_inicio = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(510, 396);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // rtb_inicio
+            // 
+            this.rtb_inicio.Location = new System.Drawing.Point(48, 49);
+            this.rtb_inicio.Name = "rtb_inicio";
+            this.rtb_inicio.Size = new System.Drawing.Size(178, 204);
+            this.rtb_inicio.TabIndex = 1;
+            this.rtb_inicio.Text = "";
+            this.rtb_inicio.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtb_inicio);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private RichTextBox rtb_inicio;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,12 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> diccionario;
+        public Form1()
+        {
+            diccionario = new Dictionary<string, int>();
+            InitializeComponent();
+        }
+    }
+}
